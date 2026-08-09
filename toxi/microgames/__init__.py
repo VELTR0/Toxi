b3159135e6c40cfd7519f40127b25958eb86af1e
VELTR0 <124514937+VELTR0@users.noreply.github.com>
2026-08-09 20:13:58 +0200

    Register microgame types

---
+++ toxi/microgames/__init__.py
@@ -0,0 +1,14 @@
+from .base import SCREEN_H, SCREEN_W
+from .sword_arena import SwordArena
+from .maze_portals import MazePortals
+from .platform_gates import PlatformGates
+from .lab_catcher import LabCatcher
+from .comet_click import CometClick
+
+MICROGAME_TYPES = {
+    "sword_arena": SwordArena,
+    "maze_portals": MazePortals,
+    "platform_gates": PlatformGates,
+    "lab_catcher": LabCatcher,
+    "comet_click": CometClick,
+}
